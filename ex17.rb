54751445fbc3b73c7114dbaa7e7dd38590bcf887
Kimbeaux <KimbeauxE@verizon.net>
2012-03-14 21:09:40 -0400

    ex17 w/ one line solution

--- ex17.rb
+++ ex17.rb
@@ -4,8 +4,10 @@ script = $0
 puts "Copying from #{from_file} to #{to_file}"
 
 # we could do these two on one line, too, how?
-input = File.open(from_file)
-indata = input.read()
+# input = File.open(from_file)
+# indata = input.read()
+# File.open(from_file).each_line{ |to_file| puts to_file }   OK, this doesn't work.
+indata = File.open(from_file){ |from_file| from_file.read}  # this works, but breaks the close on line 24.
 
 puts "The input file is #{indata.length} bytes long"
 
@@ -19,4 +21,4 @@ output.write(indata)
 puts "Alright, all done."
 
 output.close()
-input.close()
+# input.close()
