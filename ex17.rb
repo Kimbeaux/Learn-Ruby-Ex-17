75e58471d75bd7e6892ff1825ce4458f8c05ea69
Kimbeaux <KimbeauxE@verizon.net>
2012-03-15 09:15:45 -0400

    extra credit 2

--- ex17.rb
+++ ex17.rb
@@ -1,7 +1,7 @@
 from_file, to_file = ARGV
 script = $0
 
-puts "Copying from #{from_file} to #{to_file}"
+# puts "Copying from #{from_file} to #{to_file}"
 
 # we could do these two on one line, too, how?
 # input = File.open(from_file)
@@ -9,16 +9,16 @@ puts "Copying from #{from_file} to #{to_file}"
 # File.open(from_file).each_line{ |to_file| puts to_file }   OK, this doesn't work.
 indata = File.open(from_file){ |from_file| from_file.read}  # this works, but breaks the close on line 24.
 
-puts "The input file is #{indata.length} bytes long"
+# puts "The input file is #{indata.length} bytes long"
 
-puts "Does the output file exist? #{File.exists? to_file}"
-puts "Ready, hit RETURN to continue, CNTRL-C to abort."
-STDIN.gets
+# puts "Does the output file exist? #{File.exists? to_file}"
+# puts "Ready, hit RETURN to continue, CNTRL-C to abort."
+# STDIN.gets
 
 output = File.open(to_file, 'w')
 output.write(indata)
 
-puts "Alright, all done."
+puts "File #{from_file} copied to #{to_file}."
 
 output.close()
 # input.close()
